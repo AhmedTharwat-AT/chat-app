c5d0a9dbff0e71aa6319ee7fcd9862b64c4e05a5
iMorphex <ahmedsarwat3000@yahoo.com>
2024-01-18 07:47:50 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="rtl:bg-red-200 w-screen h-screen ltr:bg-blue-100 flex items-center justify-center">
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/firebase.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const authDomain = import.meta.env.VITE_authDomain;
+const projectId = import.meta.env.VITE_projectId;
+const storageBucket = import.meta.env.VITE_storageBucket;
+const messagingSenderId = import.meta.env.VITE_messagingSenderId;
+const appId = import.meta.env.VITE_appId;
+const measurementId = import.meta.env.VITE_measurementId;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { app, db };
